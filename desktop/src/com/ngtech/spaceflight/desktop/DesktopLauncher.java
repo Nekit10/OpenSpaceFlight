1b917717c73c8c1a221a38a4db0ba9f684793325
Nikita <nikitaserba@icloud.com>
2017-07-13 18:41:23 +0300

    Init commit

---
+++ desktop/src/com/ngtech/spaceflight/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.ngtech.spaceflight.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.ngtech.spaceflight.openSpaceFlight;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new openSpaceFlight(), config);
+	}
+}
