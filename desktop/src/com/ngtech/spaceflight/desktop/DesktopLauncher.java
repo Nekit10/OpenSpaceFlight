fc365d5d0d4093f61a9e018c49f39e4a83e76832
Nikita <nikitaserba@icloud.com>
2017-07-13 18:49:19 +0300

    Update desktop launcher

--- desktop/src/com/ngtech/spaceflight/desktop/DesktopLauncher.java
+++ desktop/src/com/ngtech/spaceflight/desktop/DesktopLauncher.java
@@ -4,9 +4,22 @@ import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.ngtech.spaceflight.openSpaceFlight;
 
+/**
+ * Desktop launcher. launches game from module core (Class OpenSpaceFight).
+ *
+ * @author Nikita S.
+ * @version 1.0
+ * @since 1.0
+ */
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		//Desktop launching config
+		config.fullscreen = true;
+		config.height = 1080;
+		config.width = 1920;
+
 		new LwjglApplication(new openSpaceFlight(), config);
 	}
 }
